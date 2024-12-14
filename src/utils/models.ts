783c5df4d259cd979fab274062b8dd4c68aaf34e
Breno Macedo <brenomacedo404@gmail.com>
2024-12-14 16:00:04 -0300

    Second commit

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -1,27 +1,17 @@
-import { DirectionsResponseData } from '@googlemaps/google-maps-services-js';
+import { DirectionsResponseData } from "@googlemaps/google-maps-services-js";
 
-export type DirectionsData = DirectionsResponseData & { request: any }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type DirectionsData = DirectionsResponseData & { request: any };
 
 export type RouteModel = {
     id: string;
     name: string;
-    source: {
-        name: string,
-        location: {
-            lat: number,
-            lng: number
-        }
-    },
-    destination: {
-        name: string,
-        location: {
-            lat: number,
-            lng: number
-        }
-    },
-    distance: number,
-    duration: number,
-    directions: DirectionsData,
-    created_at: Date,
-    updated_at: Date
-}
+    source: { name: string; location: { lat: number; lng: number } };
+    destination: { name: string; location: { lat: number; lng: number } };
+    distance: number;
+    duration: number;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    directions: DirectionsResponseData & { request: any };
+    created_at: Date;
+    updated_at: Date;
+};
